6a355af2c02c8e211ee20d916770847b90623aea
LeszczynskiKarol <leszczu123@wp.pl>
2026-02-11 12:52:57 +0100

    poprawka seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,26 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://1copywriting.pl',
+  site: "https://www.1copywriting.pl",
   integrations: [
     sitemap({
-      changefreq: 'weekly',
+      changefreq: "weekly",
       priority: 0.7,
       lastmod: new Date(),
     }),
   ],
   markdown: {
     shikiConfig: {
-      theme: 'github-dark',
+      theme: "github-dark",
     },
   },
   build: {
-    inlineStylesheets: 'auto',
+    inlineStylesheets: "auto",
   },
   compressHTML: true,
   prefetch: {
     prefetchAll: true,
-    defaultStrategy: 'viewport',
+    defaultStrategy: "viewport",
   },
 });
